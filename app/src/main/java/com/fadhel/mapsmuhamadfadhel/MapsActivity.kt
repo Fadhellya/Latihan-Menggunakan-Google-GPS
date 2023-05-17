6766e718b23377573c77892411d167db1817cf88
Fadhellya <muhamadfadhel50@gmail.com>
2023-05-17 10:06:59 +0700

    tambah icon

--- app/src/main/java/com/fadhel/mapsmuhamadfadhel/MapsActivity.kt
+++ app/src/main/java/com/fadhel/mapsmuhamadfadhel/MapsActivity.kt
@@ -10,10 +10,11 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.fadhel.mapsmuhamadfadhel.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    val zoom = 15F
-
+    val zoom = 13F
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
@@ -29,20 +30,67 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        val ampera = LatLng(-0.9239349776951578, 100.4427197796989)
+        var markerAmpera: Marker?=null
+        markerAmpera= mMap.addMarker(
+            MarkerOptions()
+                .position(ampera)
+                .title("Ampera Bukan Dia").snippet("Nasi Prasmanan")
+                //.icon(BitmapDescriptorFactory.defaultMarker())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.iconresto))
+        )
+        markerAmpera?.tag=0
+
+        val basko = LatLng(-0.9020027489680823, 100.35093258175712)
+        var markerBasko: Marker?=null
+        markerBasko= mMap.addMarker(
+            MarkerOptions()
+                .position(basko)
+                .title("Basko Padang").snippet("Mall Terbesar Di Padang")
+                // .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.iconmall))
+        )
+        markerBasko?.tag=0
+
+        val pa = LatLng(-0.9498494244548746, 100.35545919525188)
+        var markerPa: Marker?=null
+        markerPa= mMap.addMarker(
+            MarkerOptions()
+                .position(pa)
+                .title("Plaza Andalas").snippet("Mall yang berada di kota padang")
+                //.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.iconmall))
+        )
+        markerPa?.tag=0
+
+        val masjid = LatLng(-0.924200197227326, 100.36251101038619)
+        var markerMasjid: Marker?=null
+        markerMasjid= mMap.addMarker(
+            MarkerOptions()
+                .position(masjid)
+                .title("Masjid Raya Sumbar").snippet("Masjid yang diakui di ASEAN")
+                //.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.iconsmasjid))
+        )
+        markerMasjid?.tag=0
+        val tm = LatLng(-0.9124467671527503, 100.35738963758169)
+        var markerTm: Marker?=null
+        markerTm= mMap.addMarker(
+            MarkerOptions()
+                .position(tm)
+                .title("Transmart Padang").snippet("Mall besar yang baru di kota padang")
+                //.icon(BitmapDescriptorFactory.defaultMarker())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.iconmall))
+        )
+        markerTm?.tag=0
+
+
+
         // Add a marker in Sydney and move the camera
-        val padang = LatLng(-0.9437043, 100.3671977)
+        val padang = LatLng(-0.9436158612218682, 100.36720575469975)
         mMap.addMarker(MarkerOptions().position(padang).title("RSUP M.Jamil Padang"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(padang,zoom))
     }
